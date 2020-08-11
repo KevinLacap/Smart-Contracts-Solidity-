d775ac7f4f4412075c6802d7773371aca75d51c9
KevinLacap <61166829+KevinLacap@users.noreply.github.com>
2020-08-10 20:45:45 -0400

    Create Compile_Code.sol

---
+++ Compile_Code.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.0;
+
+contract JointSavings {
+  address payable account_one = 0x2580A5a8e9C56141AAa262b481d21Ced8AaCF9A5;
+  address payable account_two = 0x9E9451c877778f262052c3Fa78746d4981F883C4;
+  address payable account_three = 0xE85F54b1FE59eC217e7956A6e2643A48D505cBB8;
+
+  function withdraw(uint amount, address payable recipient) public {
+    return recipient.transfer(amount);
+  }
+
+  function deposit() public payable {}
+
+  function() external payable {}
+}
